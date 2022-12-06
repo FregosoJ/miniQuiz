9a5b84a23de57c6f0c4e12b7c35e3e5c4101f6b2
Jonathan Fregoso <jonathanfregoso@Jonathans-MBP.lan>
2022-12-05 17:47:42 -0800

    added submit button and reset questions

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ var sButton = document.querySelector('#start-btn');
 var timeEl = document.querySelector('.time');
 var headerEl = document.querySelector('#tittle')
 var header = document.createElement("h1");
+var timerInterval;
+
 header.textContent = "Mini Quiz";
 // Style the appended h1
 header.setAttribute("style", "display:flex; font-size: 60px; font-weight: bold; text-decoration: underLine;");
 // append it to the doc.body
 headerEl.append(header);
-
+// questions for the quiz
 var questions = [{
     question: "What does var stand for?",
     choices: ["variable", "varicosity", "variances", "vargueno"],
     answer: "variable",
 }, {
-    //make 4 question in the same format 
+    //make 4 question in the same format. done
     question: "What does the 'click' refer to?",
     choices: ["the page", "the img", "the screen", "the Button"],
     answer: "the Button",
@@ -30,21 +32,21 @@ var questions = [{
 },
 {
     question: "What can a querySelector grab from HTML?",
-    choices: ["element", "img", "<p>", "all of the above"],
+    choices: ["element", "img", "p", "all of the above"],
     answer: "all of the above",
 }
 ]
 
-var index = 0
+var currentQuestionIndex = 0
 
 function displayQuestions() {
 divTags.innerHTML = `
-<div id="question">${questions[index].question}</div>
+<div id="question">${questions[currentQuestionIndex].question}</div>
 <div id="answer-button" class="btn-grid">
-    <button class="btn">${questions[index].choices[0]}</button>
-    <button class="btn">${questions[index].choices[1]}</button>
-    <button class="btn">${questions[index].choices[2]}</button>
-    <button class="btn">${questions[index].choices[3]}</button>
+    <button class="btn">${questions[currentQuestionIndex].choices[0]}</button>
+    <button class="btn">${questions[currentQuestionIndex].choices[1]}</button>
+    <button class="btn">${questions[currentQuestionIndex].choices[2]}</button>
+    <button class="btn">${questions[currentQuestionIndex].choices[3]}</button>
 </div>
 `
 var btnEl = document.querySelectorAll('.btn')
@@ -52,19 +54,38 @@ for (i=0; i< btnEl.length; i++){
     btnEl[i].addEventListener('click', nextquestion)
 }
 }
+
 function nextquestion(){
     var userChoice = this.textContent
-    var answer = questions[index].answer
+    var answer = questions[currentQuestionIndex].answer
     if (userChoice=== answer){
         alert('correct')
     } else{
         alert('wrong')
     }
-    index++
-    if (index< questions.length){
+    console.log(currentQuestionIndex);
+    currentQuestionIndex++
+    console.log(currentQuestionIndex);
+    console.log(questions.length);
+    if (currentQuestionIndex< questions.length){
         displayQuestions()
+    } else {
+        // hide the questions
+        // displayQuestions() = hide
+        // stop the timer, done
+        clearInterval(timerInterval)
+        // give them text box and submit button
+        // show submit button 
     }
 }
+
+
+
+// add event listener for submit button
+// grab the response and grab the score put it local storage 
+
+
+// final score and there name 
 
 sButton.addEventListener('click', startGame);
 
@@ -74,7 +95,7 @@ var secondsLeft = questions.length * 15;
 
 function setTime() {
     // Sets interval in variable
-    var timerInterval = setInterval(function () {
+     timerInterval = setInterval(function () {
         if (secondsLeft > 1) {
             timeEl.textContent = secondsLeft + " Until Game Over";
             secondsLeft--;
@@ -104,5 +125,27 @@ function showResponse(event) {
     console.log(event);
     var response = "Thank you for your submission " + ".";
     submissionResponseEl.textContent = response;
+}
+
+
+function saveScore(event) {
+    event.preventDefault();
+    var initials = InputEvent.value
+    var newScore = {
+        score: timeleft,
+        initials: initials
+    }
+
+    scoreArrey.push(newScore)
+
+    localStorage.setItem("high score", JSON.stringify(scoreArrey))
+
+    scoreEl.textContent = "high score" +JSON.stringify(scoreArrey)
+
+    for (var i=0; i < scoreArrey.length; i++) {
+        var scorelist.textContent = scoreArrey[i].initials
+        formEl.append(scorelist);
+        console.log(scorelist)
+    }
 }
 
